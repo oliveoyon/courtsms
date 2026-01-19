b6a1317041824bb97b10cb70807488e65313f277
oliveoyon <arifur123@gmail.com>
2026-01-19 13:35:52 +0600

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('court:sms:dispatch')->everyMinute()->withoutOverlapping();
+        $schedule->command('sms:send-scheduled')->everyMinute()->withoutOverlapping();
     }
 
 
